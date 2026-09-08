3aeb206af99149b33d2737dd5da69a7d4dc6ea70
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-09-08 22:23:04 +0200

    feat: check database connection

--- api/app.py
+++ api/app.py
@@ -1,8 +1,25 @@
+import os
+
 from fastapi import FastAPI
 import uvicorn
+import SQLAlchemy
+import os
+
+dotenv.load_dotenv()
 
 app = FastAPI()
 
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+@app.get("/health/db")
+async def health_check_db():
+    try:
+        engine = SQLAlchemy.create_engine(os.getenv("DB_URL"))
+        connection = engine.connect()
+        SQLAlchemy.text("SELECT 1").execute(connection)
+        connection.close()
+        return {"db": "connected"}
+    except Exception as e:
+        return {"status": "unhealthy", "db": "disconnected", "error": str(e)}
