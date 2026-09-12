2258500878da79524b168d2e5d45c8f29c90dddb
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-09-12 23:41:22 +0200

    feat: add JWT creation

--- api/security.py
+++ api/security.py
@@ -1,4 +1,16 @@
+
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Any
+
+from jose import jwt
 from passlib.context import CryptContext
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']  
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +21,35 @@ def hash_password(password: str) -> str:
     if len(password.encode("utf-8")) > 72:
         raise ValueError("Password too long (max 72 bytes)")
     return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify a password against its hash.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(subject: str | Any, expires_delta: int | None = None) -> str:
+    """
+    Create a JWT access token.
+    """
+    if expires_delta is not None:
+        expires_delta = datetime.now(timezone.utc) + expires_delta
+    else:
+        expires_delta = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(subject: str | Any, expires_delta: int | None = None) -> str:
+    """
+    Create a JWT refresh token.
+    """
+    if expires_delta is not None:
+        expires_delta = datetime.now(timezone.utc) + expires_delta
+    else:
+        expires_delta = datetime.now(timezone.utc) + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
